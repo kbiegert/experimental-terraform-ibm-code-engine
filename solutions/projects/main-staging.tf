f584de819979a79174bfc96135fb1202142e4fbb
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-24 08:03:02 -0500

    add do nothing terraform for ys1-staging testing only

---
+++ solutions/projects/main-staging.tf
@@ -0,0 +1,50 @@
+
+resource "time_sleep" "wait_10_seconds" {
+    create_duration = "10s"
+}
+
+##############################################################################
+
+output "project_name_id_map" {
+    value       = "project_name_id_map"
+}
+
+output "project_1_name" {
+    value       = "project_1_name"
+}
+
+output "project_1_id" {
+    value       = "project_1_id"
+}
+
+output "project_2_name" {
+    value       = "project_2_name"
+}
+
+output "project_2_id" {
+    value       = "project_2_id"
+}
+
+output "project_3_name" {
+    value       = "project_3_name"
+}
+
+output "project_3_id" {
+    value       = "project_3_id"
+}
+
+output "project_4_name" {
+    value       = "project_4_name"
+}
+
+output "project_4_id" {
+    value       = "project_4_id"
+}
+
+output "project_5_name" {
+    value       = "project_5_name"
+}
+
+output "project_5_id" {
+    value       = "project_5_id"
+}
